48629341da5a51e9f7d691b63a0248e663852343
Natalie Anwar <na05787@ad.georgiasouthern.edu>
2023-07-06 12:25:19 -0400

    finds the best hyperparameters with the highest accuracy to use for the model & added runtime counter becuz no gpu ):

--- diabetes.py
+++ diabetes.py
@@ -10,6 +10,10 @@ from sklearn.preprocessing import OneHotEncoder
 from sklearn.compose import ColumnTransformer
 from imblearn.pipeline import Pipeline as imbPipeline
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split, GridSearchCV
+import time
+
+start_time = time.time()
 
 # Ingesting the csv dataset
 df = pd.read_csv('diabetes_prediction_dataset.csv')
@@ -117,3 +121,41 @@ clf = imbPipeline(steps=[('preprocessor', preprocessor),
                       ('classifier', RandomForestClassifier())])
 
 
+# The following finds the best hyperparameters with the highest accuracy to use for the model
+# Define the hyperparameters and the values we want to test
+param_grid = {
+    'classifier__n_estimators': [50, 100, 200],
+    'classifier__max_depth': [None, 10, 20],
+    'classifier__min_samples_split': [2, 5, 10],
+    'classifier__min_samples_leaf': [1, 2, 4]
+}
+
+
+# Create Grid Search object
+grid_search = GridSearchCV(clf, param_grid, cv=5)
+
+# Split data into train and test sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Train the model
+grid_search.fit(X_train, y_train)
+
+# Print the best parameters
+print("Best Parameters: ", grid_search.best_params_)
+
+# Convert GridSearchCV results to a DataFrame and plot
+results_df = pd.DataFrame(grid_search.cv_results_)
+plt.figure(figsize=(8, 6))
+sns.lineplot(data=results_df, x='param_classifier__n_estimators', y='mean_test_score', hue='param_classifier__max_depth', palette='viridis')
+plt.title('Hyperparameters Tuning Results')
+plt.xlabel('Number of Estimators')
+plt.ylabel('Mean Test Score')
+#plt.show()
+
+
+
+
+
+end_time = time.time()
+runtime = end_time - start_time
+print("Script runtime:", runtime, "seconds")
