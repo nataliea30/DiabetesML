b16d809231f9aea965242a07984d029f157f6002
Natalie Anwar <na05787@ad.georgiasouthern.edu>
2023-07-06 11:44:08 -0400

    grAaphing the correlation matrix heatmap and correclation with diabetes chart

--- diabetes.py
+++ diabetes.py
@@ -1,28 +1,28 @@
 import tensorflow as tf
 import numpy as np
 import pandas as pd
-
-import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 # Ingesting the csv dataset
-dataFrame = pd.read_csv('diabetes_prediction_dataset.csv')
+df = pd.read_csv('diabetes_prediction_dataset.csv')
 
 # Printing the DataFrame head and information
-print(dataFrame.head())
-dataFrame.info()
+print(df.head())
+df.info()
 
 # Dropping duplicates
-dataFrame.drop_duplicates(inplace=True)
+df.drop_duplicates(inplace=True)
 
 # Counting the number of distinct values in each column
-num_distinct_values = dataFrame.nunique()
+num_distinct_values = df.nunique()
 print(num_distinct_values)
 
 # Checking null values
-print(dataFrame.isnull().sum())
+print(df.isnull().sum())
 
 # Removing the 'Other' category from the 'gender' column
-dataFrame = dataFrame[dataFrame['gender'] != 'Other']
+df = df[df['gender'] != 'Other']
 
 # Define a function to map the existing categories to new ones in order to reduce to 3 main categories of smoking
 def recategorize_smoking(smoking_status):
@@ -34,12 +34,12 @@ def recategorize_smoking(smoking_status):
         return 'past_smoker'
 
 # Apply the function to the 'smoking_history' column
-dataFrame.loc[:, 'smoking_history'] = dataFrame['smoking_history'].apply(recategorize_smoking)
+df.loc[:, 'smoking_history'] = df['smoking_history'].apply(recategorize_smoking)
 
 # Check the new value counts
-print(dataFrame['smoking_history'].value_counts())
+print(df['smoking_history'].value_counts())
 
-data = dataFrame.copy()
+data = df.copy()
 
 
 #reaplcing the original coloumns of data to the recategorized ones
@@ -57,4 +57,24 @@ data = perform_one_hot_encoding(data, 'gender')
 
 # Perform one-hot encoding on the smoking history variable
 data = perform_one_hot_encoding(data, 'smoking_history')
+
+
+# Compute the correlation matrix
+correlation_matrix = data.corr()
+
+# Graph I: Correlation Matrix Heatmap
+plt.figure(figsize=(15, 10))
+sns.heatmap(correlation_matrix, annot=True, cmap='coolwarm', linewidths=0.5, fmt='.2f')
+plt.title("Correlation Matrix Heatmap")
+plt.show()
+
+# Graph II: Correlation with Diabetes
+target_corr_sorted = data.corr()['diabetes'].drop('diabetes').sort_values(ascending=False)
+
+sns.set(font_scale=0.8)
+sns.set_style("white")
+sns.set_palette("PuBuGn_d")
+sns.heatmap(target_corr_sorted.to_frame(), cmap="coolwarm", annot=True, fmt='.2f')
+plt.title('Correlation with Diabetes')
+plt.show()
 
