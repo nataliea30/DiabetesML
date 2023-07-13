5d4eb1da262accec6c4681eeb74ad25a564760e6
Bora Acer <boracer@hotmail.com>
2023-07-13 10:55:24 -0400

    website updated, adding complex

--- Server/app.py
+++ Server/app.py
@@ -2,17 +2,22 @@ from flask import Flask, jsonify, render_template, request
 import numpy as np
 import _pickle as cPickle
 
-print("Loading Model")
+print("Loading Simple Model")
 
-with open('Diabetes.model', 'rb') as f:
-    rf = cPickle.load(f)
+with open('simple.model', 'rb') as f:
+    rfS = cPickle.load(f)
+    
+print("Loading Complex Model")
+with open('complex.model', 'rb') as f:
+    rfC = cPickle.load(f)
     
 print("Model Loaded")
 
 app = Flask(__name__)
 
+count = 0
 
-def dataPrep(X):
+def dataPrepComplex(X):
     data = np.empty(shape=(0, 1))
     
     data = np.append(data, X["age"])
@@ -60,32 +65,109 @@ def dataPrep(X):
         data = np.append(data, 1)
         
     data = np.append(data, X["bmi"])
+    data = np.append(data, X["HbA1c_level"])
+    data = np.append(data, X["blood_glucose_level"])
+
 
     data = data.astype(float)
     print(data)
     return data.reshape(1, -1)
 
-def guess(X):
+def dataPrepSimple(X):
+    data = np.empty(shape=(0, 1))
+    
+    data = np.append(data, X["age"])
+    
+    try:
+        if X["hypertension"] == "on":
+            data = np.append(data, 1)
+    except:
+        data = np.append(data, 0)
+        
+    try:
+        if X["heart_disease"] == "on":
+            data = np.append(data, 1)
+    except:
+        data = np.append(data, 0)
+
+    if X["gender"] == "Female":
+        data = np.append(data, 1)
+        data = np.append(data, 0)
+    else:
+        print("Male")
+        data = np.append(data, 0)
+        data = np.append(data, 1)
+
+    if X["smoking_history"] == "Never":
+        print("never")
+        data = np.append(data, 0)
+        data = np.append(data, 1)
+        data = np.append(data, 0)
+    elif X["smoking_history"] == "No Info":
+        print("No Info")
+        data = np.append(data, 0)
+        data = np.append(data, 1)
+        data = np.append(data, 0)
+    elif X["smoking_history"] == "current":
+        print("current")
+        data = np.append(data, 1)
+        data = np.append(data, 0)
+        data = np.append(data, 0)
+    elif X["smoking_history"] == "former":
+        print("former")
+        data = np.append(data, 0)
+        data = np.append(data, 0)
+        data = np.append(data, 1)
+        
+    data = np.append(data, X["bmi"])
+
+    data = data.astype(float)
+    print(data)
+    return data.reshape(1, -1)
+
+def simplePredict(X):
     print("Guessing")
-    preds = rf.predict(X)
-    print(preds[0])
+    preds = rfS.predict(X)
+    return preds[0]
+
+def complexPredict(X):
+    print("Guessing")
+    print(X)
+    preds = rfC.predict(X)
     return preds[0]
 
 @app.route('/')
 def hello():
-    print("Home Accessed")
+    count =+ 1
+    print(count)
     return render_template('index.html')
 
 @app.route('/success')
 def success():
-    print("Success Accessed")
     return render_template('success.html')
 
-@app.route('/guess', methods=['POST'])
-def addOne():
+@app.route('/simple')
+def simple():
+    return render_template('simple.html')
+
+@app.route('/complex')
+def complex():
+    return render_template('complex.html')
+
+
+@app.route('/simple/guess', methods=['POST'])
+def simpleGuess():
     newGuess = request.get_json()
-    print(newGuess)
-    if guess(dataPrep(newGuess)) == 1:
+    if simplePredict(dataPrepSimple(newGuess)) == 1:
+        nGuess = "HIGH"
+    else:
+        nGuess = "LOW"
+    return jsonify({'guess' : nGuess})
+
+@app.route('/complex/guess', methods=['POST'])
+def complexGuess():
+    newGuess = request.get_json()
+    if complexPredict(dataPrepComplex(newGuess)) == 1:
         nGuess = "HIGH"
     else:
         nGuess = "LOW"
